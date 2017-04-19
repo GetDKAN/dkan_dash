200207c3e9adeee081697b18411c086af291de11
topicus <marianocarballal@gmail.com>
2017-04-19 15:16:38 -0300

    Adds an embed button to users with update permission

--- js/editor.js
+++ js/editor.js
@@ -8,7 +8,12 @@
   Drupal.behaviors.jsonEditor = {
     attach: function (context) {
       var field = document.getElementById('edit-field-dash-settings-und-0-value');
-      var json = JSON.parse(field.value);
+      var json;
+      try {
+        json = field.value ? JSON.parse(field.value) : {};
+      } catch(e) {
+        console.warn('An error ocurred trying to parse the dashboard saved value. Continue with an empty dashboard');
+      }
       var container = document.getElementById('field-dash-settings-add-more-wrapper');
       var options = {
         mode: 'tree',
